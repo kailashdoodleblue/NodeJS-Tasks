d5862852523f9d18c3280b2f7cf9c3a11aa76d1c
kailash <kailash.doodleblue@gmail.com>
2024-05-22 11:22:59 +0530

    added nodemon

--- firstnodepgm.js
+++ firstnodepgm.js
@@ -4,9 +4,15 @@ var a = require('./gettime');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   
-  res.write(a.getime()+"\n")
-  res.end('Hello World!');
+  // res.write(a.getime()+"\n")
+  res.end(htmlReturn());
 }).listen(8080); 
 
-console.log("Hello")
-console.log(a.getime())
+function htmlReturn(){
+return `
+<div>
+<p>Hello how are you</p>
+</div>`;
+}
+// console.log("Hello")
+// console.log(a.getime())
