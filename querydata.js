d1de60c449b99564d20def13bd995d56df6731ca
kailash <kailash.doodleblue@gmail.com>
2024-05-22 12:26:16 +0530

    get data from url

---
+++ querydata.js
@@ -0,0 +1,10 @@
+let http = require('http');
+let url=require('url');
+
+http.createServer (function(req,res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    let querydata=url.parse(req.url,true).query;
+    let data = querydata.name+querydata.age;
+    res.write(data);
+    res.end();
+}).listen(8000);
